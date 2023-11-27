527aeea19d2a3fe9c0b6db6b15056acd0c10d7ef
One0Heaven <omar_villeda_olguin@hotmail.com>
2023-11-27 01:48:37 -0600

    Cambio a cuestionario y lectura en php de las respuestas

--- index/phpPreguntas/index.php
+++ index/phpPreguntas/index.php
@@ -45,78 +45,348 @@
     <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>¿Cómo es una integral definida?</td>
-      <td><input type="text" class="form-control" id="R1" name="R1"></td>
+      <td class="Centrado">¿Cómo se ve una integral definida?</td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R1" name="R1" value="Tiene una integral y una funcion">Tiene una integral y una funcion
+      </label>
+      </td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R1" name="R1" value="Tiene una integral con limites y una funcion">Tiene una integral con limites y una funcion
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R1" name="R1" value="Solo es una funcion">Solo es una funcion
+      </label>
+      </td>
+
     </tr>
     <tr>
       <th scope="row">2</th>
-      <td>¿Cómo es una integral indefinida?</td>
-      <td><input type="text" class="form-control" id="R2" name="R2"></td>
+      <td>
+        ¿Cuál es una caracteristica de una integral indefinida?
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R2" name="R2" value='Se puede optener un valor como "5"'>Se puede optener un valor como "5"
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R2" name="R2" value="Se resta simpre la integral">Se resta simpre la integral
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R2" name="R2" value="No tiene limite de integracion">No tiene limite de integracion
+      </label>
+      </td>
+
     </tr>
     <tr>
       <th scope="row">3</th>
-      <td>¿Qué es la integración por cambio de variable?</td>
-      <td><input type="text" class="form-control" id="R3" name="R3"></td>
+      <td>Paso importante en la integración por cambio de variable</td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R3" name="R3" value='Obtener "v" "dv" y "dx"'>Obtener "v" "dv" y "dx"
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R3" name="R3" value="Separar los numeros reales">Separar los numeros reales
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R3" name="R3" value="Pedirle al Manu que lo resuelva">Pedirle al Manu que lo resuelva
+      </label>
+      </td>
     </tr>
+
     <tr>
       <th scope="row">4</th>
-      <td>¿Qué es la integración por trigonometría?</td>
-      <td><input type="text" class="form-control" id="R4" name="R4"></td>
-    </tr>
+      <td>¿Qué nos conviene más hacer en las Integrales por cambio de variable?</td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R4" name="R4" value="Integrar las inmediatas">Integrar las inmediatas
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R4" name="R4" value="Factorizar la funcion">Factorizar la funcion
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R4" name="R4" value='Tratar de obtener un "ln" o una exponencial'>Tratar de obtener un "ln" o una exponencial
+      </label>
+      </td>
+
     <tr>
       <th scope="row">5</th>
-      <td>¿Qué es una integral impropia?</td>
-      <td><input type="text" class="form-control" id="R5" name="R5"></td>
+      <td>¿Qué son los límites de integración?</td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R5" name="R5" value="Posibles resultados">Posibles resultados
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R5" name="R5" value="Los exponentes a aplicar a la funcion">Los exponentes a aplicar a la funcion
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R5" name="R5" value="Los valores entre los que se integra la funcion">Los valores entre los que se integra la funcion
+      </label>
+      </td>
+
     </tr>
     <tr>
       <th scope="row">6</th>
-      <td>¿Qué es una integral doble?</td>
-      <td><input type="text" class="form-control" id="R6" name="R6"></td>
+      <td>La regla de Barrow es para...</td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R6" name="R6" value="Resolver todas las integrales">Resolver todas las integrales
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R6" name="R6" value="Resolver intergrales definidas">Resolver intergrales definidas
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R6" name="R6" value="Hacerme un sandwich">Hacerme un sandwich
+      </label>
+      </td>
+
     </tr>
     <tr>
       <th scope="row">7</th>
-      <td>¿Qué es una integral triple?</td>
-      <td><input type="text" class="form-control" id="R7" name="R7"></td>
+      <td>Integral de una constante</td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R7" name="R7" value="La constante por 'x'">La constante por "x"
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R7" name="R7" value="Cero">Cero
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R7" name="R7" value="La constante al cuadrado">La constante al cuadrado
+      </label>
+      </td>
     </tr>
+
     <tr>
       <th scope="row">8</th>
-      <td>¿Qué es una integral de línea?</td>
-      <td><input type="text" class="form-control" id="R8" name="R8"></td>
+      <td>Integral de: <span class="math">\[x\]</span></td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R8" name="R8"><span class="math" value="frac {x}{2}">\[\frac {x}{2}\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R8" name="R8"><span class="math" value="frac {x^2}{2}">\[\frac {x^2}{2}\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R8" name="R8" value="No se puede resolver">No se puede resolver
+      </label>
+      </td>
     </tr>
+
     <tr>
       <th scope="row">9</th>
-      <td>¿Qué es una integral de superficie?</td>
-      <td><input type="text" class="form-control" id="R9" name="R9"></td>
+      <td>Integral de: <span class="math">\[\frac {1}{x}\]</span></td>
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R9" name="R9" value="frac {1}{x}"><span class="math">\[\frac {1}{x}\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R9" name="R9" value="ln \lvert x\rvert"><span class="math">\[\ln \lvert x\rvert\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R9" name="R9" value="x"><span class="math">\[x\]</span>
+      </label>
+      </td> 
     </tr>
+
     <tr>
       <th scope="row">10</th>
-      <td>¿Qué es una integral elíptica?</td>
-      <td><input type="text" class="form-control" id="R10" name="R10"></td>
+      <td>Integral de: <span class="math">\[\cos(x)\]</span></td></td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R10" name="R10" value="tan(x)"><span class="math">\[\tan(x)\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R10" name="R10" value="operatorname{sen}(x)"><span class="math">\[\operatorname{sen}(x)\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R10" name="R10" value="-ln \lvert\cos(x)\rvert "><span class="math">\[-\ln \lvert\cos(x)\rvert \]</span>
+      </label>
+      </td>
     </tr>
+
     <tr>
       <th scope="row">11</th>
-      <td>¿Qué es el teorema fundamental del cálculo?</td>
-      <td><input type="text" class="form-control" id="R11" name="R11"></td>
+      <td>Integral de: <span class="math">\[e^x\]</span></td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R11" name="R11" value="ex"><span class="math">\(ex\)</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R11" name="R11" value="e^x"><span class="math">\(e^x\)</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R11" name="R11" value="No se puede integrar">No se puede integrar
+      </label>
+      </td>
     </tr>
+
     <tr>
       <th scope="row">12</th>
-      <td>¿Qué es la integración por fracciones parciales?</td>
-      <td><input type="text" class="form-control" id="R12" name="R12"></td>
+      <td>Integral de: <span class="math">\[a^x\]</span></td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R12" name="R12" value="a"><span class="math">\[a\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R12" name="R12" value="ln(a)"><span class="math">\[\ln(a)\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R12" name="R12" value="a^x ln(a)"><span class="math">\[a^x/\ln(a)\]</span>
+      </label>
+      </td>
     </tr>
+
     <tr>
       <th scope="row">13</th>
-      <td>¿Qué es una regla de integración?</td>
-      <td><input type="text" class="form-control" id="R13" name="R13"></td>
+      <td>Integral de: <span class="math">\[\sec^2(x)\]</span></td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R13" name="R13" value="-cos(x)"><span class="math">\[-\cos(x)\]</span>
+      </label>
+      </td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R13" name="R13" value="sen(x)"><span class="math">\[\operatorname{sen}(x)\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R13" name="R13" value="tan(x)"><span class="math">\[\tan(x)\]</span>
+      </label>
+      </td>
     </tr>
+
     <tr>
       <th scope="row">14</th>
-      <td>¿Qué es la integración por partes?</td>
-      <td><input type="text" class="form-control" id="R14" name="R14"></td>
+      <td>Integral de: <span class="math">\[\text{cosec}^2(x)\]</span></td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R14" name="R14" value="-cotan(x)"><span class="math">\[-\text{cotan}(x)\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R14" name="R14" value="arcsen(x)"><span class="math">\[\operatorname{arcsen}(x)\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R14" name="R14" value="arccos(x)"><span class="math">\[\arccos(x)\]</span>
+      </label>
+      </td>
     </tr>
+
     <tr>
       <th scope="row">15</th>
-      <td>¿Qué es la sustitución?</td>
-      <td><input type="text" class="form-control" id="R15" name="R15"></td>
+      <td>Integral de: <span class="math">\[\frac{1}{\sqrt{1-x^2}}\]</span></td>
+      
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R15" name="R15" value="operatornamearcsen(x)"><span class="math">\[\operatorname{arcsen}(x)\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R15" name="R15" value="arctan(x)"><span class="math">\[\arctan(x)\]</span>
+      </label>
+      </td>
+
+      <td>
+      <label>
+        <input type="radio" class="form-control" id="R15" name="R15" value="cos(x)"><span class="math">\[\cos(x)\]</span>
+      </label>
+      </td>
+
     </tr>
   
     </tbody>
@@ -128,6 +398,14 @@
 
     </form>
 </div>
+
+
+
+<style>
+  .centrado {
+    text-align: center;
+  }
+</style>
 
 
 <?php 
